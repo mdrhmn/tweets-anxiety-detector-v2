6636734f2f265a7ffdb5140b656f10a52abcd5e9
mdrhmn <mdrhmn99@gmail.com>
2023-02-20 22:31:44 +0800

    feat: added custom config

---
+++ services/frontend/config.js
@@ -0,0 +1,19 @@
+// Upload files in static/ So any file static/file.pdf -> /file.pdf is accessible at root
+
+export default {
+	name: 'itsmeray',
+	firstName: 'Muhammad Rahiman',
+	fullName: 'Muhammad Rahiman Abdulmanab',
+	domain: 'tweets-anxiety-predictor.herokuapp.com', // add without https://, used in meta tags and share urls
+	url: 'https://tweets-anxiety-predictor.herokuapp.com/', // add without https://, used in meta tags and share urls
+	email: 'mdrhmn99@gmail.com',
+
+	social: {
+		github: 'mdrhmn',
+		linkedin: 'muhd-rahiman',
+		// facebook: 'Muhd Rahiman',
+		twitter: 'MuhdRahiman_',
+		instagram: 'rayperture',
+		codestats: 'mdrhmn' // https://codestats.net make a profile if you dont already have one.
+	},
+}
