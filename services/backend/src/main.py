0ce63e93b02b68c53bfa09c921689f764fc5004b
mdrhmn <mdrhmn99@gmail.com>
2023-02-21 00:30:53 +0800

    fix: used custom cors middleware

--- services/backend/src/main.py
+++ services/backend/src/main.py
@@ -22,10 +22,33 @@ warnings.filterwarnings("ignore")
 
 app = FastAPI()
 # handler = Mangum(app)
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=["http://localhost:3000", "http://127.0.0.1:3000",
+#                    "https://tweets-anxiety-predictor.vercel.app/"],
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
+
+origins = [
+    "http://localhost:3000", "http://127.0.0.1:3000",
+    "https://tweets-anxiety-predictor.vercel.app/"
+]
+
+
+@app.middleware("http")
+async def add_cors_headers(request, call_next):
+    response = await call_next(request)
+    response.headers["Access-Control-Allow-Origin"] = origins
+    response.headers["Access-Control-Allow-Credentials"] = "true"
+    response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
+    response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
+    return response
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000",
-                   "https://tweets-anxiety-predictor.vercel.app/"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
