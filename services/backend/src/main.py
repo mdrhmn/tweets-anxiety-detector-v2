fa91079c1fc2ab8558a5163dc0413b48b0bb55e8
mdrhmn <mdrhmn99@gmail.com>
2023-02-21 23:18:22 +0800

    fix: updated api

--- services/backend/src/main.py
+++ services/backend/src/main.py
@@ -58,11 +58,11 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=ALLOWED_ORIGINS,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
-    # expose_headers=["*"],
+    expose_headers=["*"],
 )
 
 
