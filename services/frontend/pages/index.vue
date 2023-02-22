5842f1c3c6ddb94fc06c4c0aa4bde4d135c035d9
mdrhmn <mdrhmn99@gmail.com>
2023-02-23 01:35:32 +0800

    feat: updated ui/ux

--- services/frontend/pages/index.vue
+++ services/frontend/pages/index.vue
@@ -294,14 +294,17 @@
           </div>
 
           <div
-            v-if="isProcessing"
             class="lg:max-w-xl p-4 my-5 text-sm dark:text-white rounded-md relative py-3 mb-10 w-full items-center justify-center bg-white border border-gray-100 shadow-md dark:bg-gray-500 dark:border-gray-800 dark:hover:bg-gray-400"
           >
             <h5
               class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white opacity-20"
             >
-              Processing, please wait...
+              Analysing tweet...
             </h5>
+            <p class="font-normal text-gray-700 dark:text-white opacity-20">
+              Please wait
+            </p>
+
             <div
               role="status"
               class="absolute -translate-x-1/2 -translate-y-1/2 top-2/4 left-1/2"
